eab922f888cce9b277b3b396e81d3833a69559c4
Sangyun Ock <demibun@a41ventures.com>
2022-05-25 20:54:17 +0900

    fix: submessage reply

--- src/contract.rs
+++ src/contract.rs
@@ -4,15 +4,19 @@ use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::state::{Config, CONFIG};
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_binary, Binary, Deps, DepsMut,  Env, MessageInfo, Response, StdResult, Uint128, WasmMsg, SubMsg};
+use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint128, WasmMsg, SubMsg, Reply, Addr};
 use cw2::set_contract_version;
 use cw20::{Cw20Coin, MinterResponse};
 use cw20_base::msg::InstantiateMsg as Cw20InstantiateMsg;
 use cw721_base::msg::InstantiateMsg as Cw721InstantiateMsg;
+use cw_utils::parse_reply_instantiate_data;
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:cosmonaut-contract";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+const CW20_CONTRACT_REPLY_ID: u64 = 1;
+const CW721_CONTRACT_REPLY_ID: u64 = 2;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -48,7 +52,7 @@ pub fn instantiate(
         })?,
         funds: vec![],
         label: "mars token for money".to_string(),
-    }, 0);
+    }, CW20_CONTRACT_REPLY_ID);
 
     let instantiate_cw721_contract: SubMsg = SubMsg::reply_on_success(WasmMsg::Instantiate {
         admin: Option::from(info.sender.to_string()),
@@ -60,12 +64,41 @@ pub fn instantiate(
         })?,
         funds: vec![],
         label: "spaceship nft".to_string(),
-    }, 1);
+    }, CW721_CONTRACT_REPLY_ID);
 
     Ok(Response::new()
         .add_submessages([instantiate_cw20_contract, instantiate_cw721_contract])
         .add_attribute("action", "instantiate")
         .add_attribute("sender", info.sender))
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn reply(deps: DepsMut, _env: Env, msg: Reply) -> StdResult<Response> {
+    match msg.id {
+        CW20_CONTRACT_REPLY_ID => handle_instantiate_reply(deps, msg),
+        CW721_CONTRACT_REPLY_ID => handle_instantiate_reply(deps, msg),
+        id => Err(StdResult::generic_err(format!("Unknown reply id: {}", id))),
+    }
+}
+
+fn handle_instantiate_reply(deps: DepsMut, msg: Reply) -> StdResult<Response> {
+    let res = parse_reply_instantiate_data(msg)?;
+    match msg.id {
+        CW20_CONTRACT_REPLY_ID => {
+            CONFIG.update(deps.storage, |mut config| -> StdResult<_> {
+                config.money_cw20_contract.addr = Option::from(Addr::unchecked(res.contract_address));
+                Ok(config)
+            })
+        }
+        CW721_CONTRACT_REPLY_ID => {
+            CONFIG.update(deps.storage, |mut config| -> StdResult<_> {
+                config.spaceship_cw721_contract.addr = Option::from(Addr::unchecked(res.contract_address));
+                Ok(config)
+            })
+        }
+        _ => {}
+    }
+    Ok(Response::new())
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
