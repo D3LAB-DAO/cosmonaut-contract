ff83d1caaac18cbde2c845ed11a34c3cbf1036fb
SangyunOck <demibun@a41Ventures.com>
2022-07-14 15:57:09 +0900

    chore: script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v ${HOME}/.cargo/registry:/usr/local/cargo/registry cosmonaut:1.0.0 bash
+docker run --rm -it -v ${HOME}/.cargo/registry:/usr/local/cargo/registry cosmonaut:1.0.0 1
